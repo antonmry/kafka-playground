052a4e8dec353fcc0e3dd908dddd85a7b22fd119
Anton Rodriguez <anton@galiglobal.com>
2022-08-22 17:03:00 +0200

    Added OTel Java agent Kafka metrics

---
+++ otel-poc/lib/download.sh
@@ -0,0 +1,1 @@
+wget https://github.com/open-telemetry/opentelemetry-java-instrumentation/releases/download/v1.17.0/opentelemetry-javaagent.jar
--- otel-poc/src/main/java/io/confluent/examples/clients/cloud/ProducerExample.java
+++ otel-poc/src/main/java/io/confluent/examples/clients/cloud/ProducerExample.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2020 Confluent Inc.
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,8 @@
  */
 package io.confluent.examples.clients.cloud;
 
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -26,12 +28,15 @@ import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 
+import io.opentelemetry.instrumentation.kafkaclients.KafkaTelemetry;
 import io.confluent.examples.clients.cloud.model.DataRecord;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+
 import org.apache.kafka.common.errors.TopicExistsException;
+
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -42,78 +47,81 @@ import java.util.Optional;
 
 public class ProducerExample {
 
-  // Create topic in Confluent Cloud
-  public static void createTopic(final String topic,
-                          final Properties cloudConfig) {
-      final NewTopic newTopic = new NewTopic(topic, Optional.empty(), Optional.empty());
-      try (final AdminClient adminClient = AdminClient.create(cloudConfig)) {
-          adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
-      } catch (final InterruptedException | ExecutionException e) {
-          // Ignore if TopicExistsException, which may be valid if topic exists
-          if (!(e.getCause() instanceof TopicExistsException)) {
-              throw new RuntimeException(e);
-          }
-      }
-  }
-
-  public static void main(final String[] args) throws IOException {
-    if (args.length != 2) {
-      System.out.println("Please provide command line arguments: configPath topic");
-      System.exit(1);
-    }
-
-    // Load properties from a local configuration file
-    // Create the configuration file (e.g. at '$HOME/.confluent/java.config') with configuration parameters
-    // to connect to your Kafka cluster, which can be on your local host, Confluent Cloud, or any other cluster.
-    // Follow these instructions to create this file: https://docs.confluent.io/platform/current/tutorials/examples/clients/docs/java.html
-    final Properties props = loadConfig(args[0]);
-
-    // Create topic if needed
-    final String topic = args[1];
-    //createTopic(topic, props);
-
-    // Add additional properties.
-    props.put(ProducerConfig.ACKS_CONFIG, "all");
-    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
-
-    Producer<String, DataRecord> producer = new KafkaProducer<String, DataRecord>(props);
-
-    // Produce sample data
-    final Long numMessages = 1_0L;
-    for (Long i = 0L; i < numMessages; i++) {
-      String key = "alice";
-      DataRecord record = new DataRecord(i);
-
-      System.out.printf("Producing record: %s\t%s%n", key, record);
-      producer.send(new ProducerRecord<String, DataRecord>(topic, key, record), new Callback() {
-          @Override
-          public void onCompletion(RecordMetadata m, Exception e) {
-            if (e != null) {
-              e.printStackTrace();
-            } else {
-              System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+    // Create topic in Confluent Cloud
+    public static void createTopic(final String topic,
+                                   final Properties cloudConfig) {
+        final NewTopic newTopic = new NewTopic(topic, Optional.empty(), Optional.empty());
+        try (final AdminClient adminClient = AdminClient.create(cloudConfig)) {
+            adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+        } catch (final InterruptedException | ExecutionException e) {
+            // Ignore if TopicExistsException, which may be valid if topic exists
+            if (!(e.getCause() instanceof TopicExistsException)) {
+                throw new RuntimeException(e);
             }
-          }
-      });
+        }
     }
 
-    producer.flush();
+    public static void main(final String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Please provide command line arguments: configPath topic");
+            System.exit(1);
+        }
 
-    System.out.printf("10 messages were produced to topic %s%n", topic);
+        // Load properties from a local configuration file
+        // Create the configuration file (e.g. at '$HOME/.confluent/java.config') with configuration parameters
+        // to connect to your Kafka cluster, which can be on your local host, Confluent Cloud, or any other cluster.
+        // Follow these instructions to create this file: https://docs.confluent.io/platform/current/tutorials/examples/clients/docs/java.html
+        final Properties props = loadConfig(args[0]);
 
-    producer.close();
-  }
+        // Create topic if needed
+        final String topic = args[1];
+        //createTopic(topic, props);
 
-  public static Properties loadConfig(final String configFile) throws IOException {
-    if (!Files.exists(Paths.get(configFile))) {
-      throw new IOException(configFile + " not found.");
+        // Add additional properties.
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
+
+        KafkaTelemetry kafkaTelemetry = KafkaTelemetry.create(GlobalOpenTelemetry.get());
+        props.putAll(kafkaTelemetry.metricConfigProperties());
+
+        Producer<String, DataRecord> producer = new KafkaProducer<String, DataRecord>(props);
+
+        // Produce sample data
+        final Long numMessages = 1_0L;
+        for (Long i = 0L; i < numMessages; i++) {
+            String key = "alice";
+            DataRecord record = new DataRecord(i);
+
+            System.out.printf("Producing record: %s\t%s%n", key, record);
+            producer.send(new ProducerRecord<String, DataRecord>(topic, key, record), new Callback() {
+                @Override
+                public void onCompletion(RecordMetadata m, Exception e) {
+                    if (e != null) {
+                        e.printStackTrace();
+                    } else {
+                        System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+                    }
+                }
+            });
+        }
+
+        producer.flush();
+
+        System.out.printf("10 messages were produced to topic %s%n", topic);
+
+        producer.close();
     }
-    final Properties cfg = new Properties();
-    try (InputStream inputStream = new FileInputStream(configFile)) {
-      cfg.load(inputStream);
+
+    public static Properties loadConfig(final String configFile) throws IOException {
+        if (!Files.exists(Paths.get(configFile))) {
+            throw new IOException(configFile + " not found.");
+        }
+        final Properties cfg = new Properties();
+        try (InputStream inputStream = new FileInputStream(configFile)) {
+            cfg.load(inputStream);
+        }
+        return cfg;
     }
-    return cfg;
-  }
 
 }
